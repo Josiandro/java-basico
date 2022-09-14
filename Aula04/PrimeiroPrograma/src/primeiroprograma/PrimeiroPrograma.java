2a615dc9799024cd55f67e40ca0a64346fd55305
Josiandro <josiandro.santossilveira@hotmail.com>
2022-09-13 22:33:00 -0300

    OlaMundo
    Primeiro programa em Java

---
+++ Aula04/PrimeiroPrograma/src/primeiroprograma/PrimeiroPrograma.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package primeiroprograma; // 1º Pacote
+
+public class PrimeiroPrograma { // 2º Classe
+
+    public static void main(String[] args) { // 3º Método principal
+        System.out.println("Olá, Mundo!");
+    }
+    
+}
+
+/*
+ * psvm + Tab vai gerar o public static void main(String[] args) {
+ * sout + Tab vai gerar o System.out.println();
+ */
