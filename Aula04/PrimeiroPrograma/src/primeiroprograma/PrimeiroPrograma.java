a74dc52c58a6eb531125e5aa13b9a15e27728797
Josiandro <josiandro.santossilveira@hotmail.com>
2022-09-13 22:51:50 -0300

    Update PrimeiroPrograma.java

--- Aula04/PrimeiroPrograma/src/primeiroprograma/PrimeiroPrograma.java
+++ Aula04/PrimeiroPrograma/src/primeiroprograma/PrimeiroPrograma.java
@@ -2,14 +2,12 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
-package primeiroprograma; // 1º Pacote
+package primeiroprograma; // Pacote primeiroprograma
 
-public class PrimeiroPrograma { // 2º Classe
-
-    public static void main(String[] args) { // 3º Método principal
-        System.out.println("Olá, Mundo!");
-    }
-    
+public class PrimeiroPrograma { // Classe PrimeiroPrograma
+    public static void main(String[] args) { // Método main        
+        System.out.println("Olá, Mundo!"); // Classe System        
+    }    
 }
 
 /*
