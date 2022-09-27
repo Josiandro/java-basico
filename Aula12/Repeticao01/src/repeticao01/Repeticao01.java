e0311567a1523eb56f66c0090b84281f3247a17c
Josiandro <josiandro.santossilveira@hotmail.com>
2022-09-26 22:02:33 -0300

    Aula 12

---
+++ Aula12/Repeticao01/src/repeticao01/Repeticao01.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package repeticao01;
+
+/**
+ *
+ * @author josia
+ */
+public class Repeticao01 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int cc = 0;
+        
+        do { // do = faça
+            System.out.println("Cabalhota " + cc);
+            cc++;
+        } while (cc<4); // Teste lógico do enquanto é feito no final
+    }
+    
+}
