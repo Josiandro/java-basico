1f2480ee845a8f46697a23aec72bb87bb472f728
Josiandro <josiandro.santossilveira@hotmail.com>
2022-10-03 22:15:28 -0300

    Exercícios 2 e 3

---
+++ Aula13/Exercicio2/src/exercicio2/Exercicio2.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package exercicio2;
+
+/**
+ *
+ * @author josia
+ */
+public class Exercicio2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int cont = 0;
+        
+        for (int c=33; c>13; c-=2) {
+            cont++;
+            System.out.println(cont);
+        }
+    }
+    
+}
---
+++ Aula13/Exercicio3/src/exercicio3/Exercicio3.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package exercicio3;
+
+/**
+ *
+ * @author josia
+ */
+public class Exercicio3 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        for (int i=0; i<=15; i+=2) {
+            if (i%3==0) continue;
+            System.out.print(" " + i);
+        }
+    }
+    
+}
